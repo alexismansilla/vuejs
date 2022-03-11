b5d9c3804903e35bd9bcc96427cddaab3578425f
Alexis Mansilla <alexis.mansilla04@gmail.com>
2022-03-10 23:27:09 -0300

    Add auth with firebase.

--- vue-payroll-manager/src/App.vue
+++ vue-payroll-manager/src/App.vue
@@ -4,3 +4,19 @@
   </div>
   <router-view/>
 </template>
+
+<script>
+import { onMounted } from "vue"
+import { auth } from "./utils/firebase"
+
+export default {
+  name: "App",
+  setup() {
+    onMounted(() => {
+      auth.onAuthStateChanged((user) => {
+        console.log(user)
+      })
+    })
+  }
+}
+</script>
